60d25c99e1a9f3f923af0057267c3f89b1c19bb6
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-03-10 23:51:00 +0900

    feat: ギルド参加時にオーナーにウェルカムメッセージを送信する機能を追加

--- src/main.py
+++ src/main.py
@@ -13,6 +13,8 @@ DB_DSN = os.getenv("MONGODB_DSN")
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.guilds = True
+intents.members = True
 
 
 class AnalyzerBot(commands.Bot):
@@ -84,6 +86,60 @@ async def on_message(message):
     bot.db.messages.insert_one(data)
 
     await bot.process_commands(message)
+
+
+@bot.event
+async def on_guild_join(guild):
+    print(f"Joined guild: {guild.name} (ID: {guild.id})")
+    try:
+        owner = guild.owner  # サーバーオーナー
+
+        if owner is None:
+            print(f"{guild.name} のオーナー情報が取得できませんでした")
+            return
+
+        message = """
+# Analyze Botをご利用いただきありがとうございます :tada:
+Analyze Botは、サーバー内のメッセージを分析して、様々な統計情報を提供するDiscord Botです。
+以下のコマンドを使用して、サーバーの分析を開始できます。
+- `/ping`: Botの応答速度を確認します。
+- `/wordcloud`: サーバー内の頻出単語をワードクラウド形式で表示します。
+- `/about`: Botのバージョンや開発者情報を表示します。
+- `/optout`: 統計データからのオプトアウト設定を行います。
+ご質問やフィードバックがある場合は、開発者までお気軽にお問い合わせください。
+
+## :warning: 注意点とサーバーオーナーのみなさまへのお願い
+Analyze Botは、Discordサーバー内のメッセージを分析するため、プライバシーに配慮した設計となっていますが、以下の点にご注意ください。
+また、下記内容をDiscordサーバー内のメンバーに広く周知していただくことを推奨します。(@everyoneなどを利用して告知してください。)
+1. **データ収集の範囲**: Analyze Botは、サーバー内のテキストチャンネルのメッセージを収集します。
+2. **データの保存期間**: 収集されたメッセージデータは、30日間保存され、その後自動的に削除されます。
+3. **ユーザーのオプトアウト**: ユーザーは、`/optout` コマンドを使用して、統計データからオプトアウトすることができます。オプトアウトされたユーザーのメッセージは、分析の対象外となります。
+4. **サーバーオーナーの責任**: サーバーオーナーは、Analyze Botの使用に関して、サーバー内のメンバーに適切な説明を行い、必要に応じて同意を得ることを推奨します。
+Analyze Botは、ユーザーのプライバシーを尊重し、データの安全な取り扱いに努めていますが、サーバーオーナーの皆様には、Botの使用に関する透明性を保ち、メンバーの信頼を得るための適切な対応をお願い申し上げます。
+
+### 周知用テンプレート
+以下は、サーバー内での周知用テンプレートの例です。
+メンバーの皆様にAnalyze Botの導入とプライバシーに関する注意点を周知する際にご活用ください。
+```
+## Analyze Bot導入のお知らせ :tada:
+@everyone この度、当サーバーではAnalyze Botを導入しました！Analyze Botは、サーバー内のメッセージを分析して、様々な統計情報を提供するDiscord Botです。
+### Analyze Botの主な機能
+- サーバー内の頻出単語をワードクラウド形式で表示
+- メッセージの送信頻度やアクティブな時間帯の分析
+- ユーザーごとのメッセージ数やアクティブ度の分析
+### プライバシーに関する注意点
+Analyze Botは、サーバー内のテキストチャンネルのメッセージを収集しますが、収集されたデータは30日間保存され、その後自動的に削除されます。
+また、ユーザーは`/optout`コマンドを使用して、統計データからご自身のメッセージをオプトアウトすることができます。オプトアウトされたユーザーのメッセージは、分析の対象外となります。
+詳しくは、[プライバシー・ポリシー](https://analyze-bot.uniproject.jp/privacy )をご覧ください。
+```
+"""
+        await owner.send(message)
+    except discord.Forbidden:
+        print(f"{guild.name} のオーナーに権限不足のためDMを送れませんでした")
+    except Exception as e:
+        print(
+            f"{guild.name} へのウェルカムメッセージの送信中にエラーが発生しました: {e}"
+        )
 
 
 async def main():
